6be3d4a1be6cc10dec5b30c548cdfe489d590c0e
DaichiYoshimura <flatsdy0819@gamil.com>
2020-12-05 20:35:52 +0900

    [update] set method

--- internal/tonality.go
+++ internal/tonality.go
@@ -1,5 +1,6 @@
 package tonality
 
+// Tonality :
 type Tonality struct {
 	tonal tonal
 	key   key
@@ -19,30 +20,30 @@ func (tonal *tonal) Sign() string {
 	return tonal.sign
 }
 
-func (tonal *tonal) SetSign() string {
-	return tonal.sign
+func (tonal *tonal) SetSign(sign string){
+	tonal.sign = sign
 }
 
 func (tonal *tonal) Index() uint {
 	return tonal.index
 }
 
-func (tonal *tonal) SetIndex() uint {
-	return tonal.index
+func (tonal *tonal) SetIndex(index uint){
+	tonal.index = index
 }
 
 func (key *key) Sign() string {
 	return key.sign
 }
 
-func (key *key) SetSign() string {
-	return key.sign
+func (key *key) SetSign(sign string){
+	key.sign = sign
 }
 
 func (key *key) Index() uint {
 	return key.index
 }
 
-func (key *key) SetIndex() uint {
-	return key.index
+func (key *key) SetIndex(index uint){
+	key.index = index
 }
